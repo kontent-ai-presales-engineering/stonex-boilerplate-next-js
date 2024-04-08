683f80db8bda094bfc724ec042b684260f86fae6
Maarten van den Hooven <maartenv@kontent.ai>
2024-04-08 12:44:36 +0200

    fixed app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,16 @@
 import '../styles/globals.css'
 import Head from 'next/head'
 import { AppProps } from 'next/app'
-import { Html } from 'next/document'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="layout">
-        <Head>
-          <title>Kontent.ai Next.js boilerplate</title>
-          <meta name='description' content="Discover the fascinating world of Dancing Goat high-quality coffee and you will never miss a single coffee break again." />
-          <link rel="icon" href="/favicon.png" />
-        </Head>
+      <Component {...pageProps} />
+      <Head>
+        <title>Kontent.ai Next.js boilerplate</title>
+        <meta name='description' content="Discover the fascinating world of Dancing Goat high-quality coffee and you will never miss a single coffee break again." />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
     </div>
   )
 }
-

d4eee930b978913bd855dce0a0b721d0e7323f8a
Maarten van den Hooven <maartenv@kontent.ai>
2024-04-08 12:41:18 +0200

    add i18n

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,11 @@ import { Html } from 'next/document'
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="layout">
-      <Component {...pageProps} />
-      <Html lang="en-gb">
         <Head>
           <title>Kontent.ai Next.js boilerplate</title>
           <meta name='description' content="Discover the fascinating world of Dancing Goat high-quality coffee and you will never miss a single coffee break again." />
           <link rel="icon" href="/favicon.png" />
         </Head>
-      </Html>
     </div>
   )
 }

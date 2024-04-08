edf1205afa96e9bd475adb2349fcf4b9e3c1b46a
Maarten van den Hooven <maartenv@kontent.ai>
2024-04-08 12:35:43 +0200

    add metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
 import '../styles/globals.css'
 import Head from 'next/head'
 import { AppProps } from 'next/app'
+import { Html } from 'next/document'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="layout">
       <Component {...pageProps} />
-      <Head>
-        <title>Kontent.ai Next.js boilerplate</title>
-        <link rel="icon" href="/favicon.png" />
-      </Head>
+      <Html lang="en-gb">
+        <Head>
+          <title>Kontent.ai Next.js boilerplate</title>
+          <meta name='description' content="Discover the fascinating world of Dancing Goat high-quality coffee and you will never miss a single coffee break again." />
+          <link rel="icon" href="/favicon.png" />
+        </Head>
+      </Html>
     </div>
   )
 }
